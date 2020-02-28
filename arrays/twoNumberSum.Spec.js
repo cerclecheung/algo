1468d757a0d4ae7957709408f85edfd586169d05
Yuanhao Zhang <cercle@bu.edu>
2020-02-27 21:25:38 -0500

    fixed spec syntax

--- arrays/twoNumberSum.Spec.js
+++ arrays/twoNumberSum.Spec.js
@@ -1,9 +1,6 @@
-/* eslint-env jasmine */
-/* eslint-disable no-undef */
-
 describe("hahahha", () => {
   it("blah", function() {
     const output = twoNumberSum([4, 6], 10).sort((a, b) => a - b);
-    expect(output).to.deep.equal([4, 6]);
+    expect(output).toEqual([4, 6]);
   });
 });
